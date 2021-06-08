d65534aa171254a625ee74ea94a85cd991f73e97
Oleg Efimenko <50793224+Navirator@users.noreply.github.com>
2021-06-08 18:39:21 +0300

    Add files via upload

--- Gacha_bot/main.rb
+++ Gacha_bot/main.rb
@@ -13,20 +13,49 @@ t = Time.now
 i = 1
 f = 1
 bot.command :день_рождения do |mgs|
-  while i <= 3
-    #d = db.query "SELECT BirthdayDay FROM 'Ships' WHERE ID==1"
-    #m = db.query "SELECT BirthdayMonth FROM 'Ships' WHERE ID==1"
+  while i <= 250
+    d = nil
+    m = nil
 
     if f == 250
       mgs.respond "Сегодня нет ни у кого день рождения("
     end
 
-    #if d == t.day && m == t.month
-      name = db.query "SELECT ShipName FROM 'Ships' WHERE ID==1"
-      mgs.respond "Сегодня день рождения #{t.day.to_s}/#{t.month.to_s} y #{name}"
-    #else
-    #  f +=1
-    #end
+    name = db.execute <<-SQL
+       SELECT ShipName FROM Ships WHERE ID==#{i}
+    SQL
+
+    day = db.execute <<-SQL
+       SELECT BirthdayDay FROM Ships WHERE ID==#{i}
+    SQL
+
+    day.each do |row|
+      d = row['BirthdayDay']
+    end
+
+    month = db.execute <<-SQL
+       SELECT BirthdayMonth FROM Ships WHERE ID==#{i}
+    SQL
+
+    month.each do |row|
+      m = row['BirthdayMonth']
+    end
+
+    year = db.execute <<-SQL
+       SELECT BirthdayYear FROM Ships WHERE ID==#{i}
+    SQL
+
+    if d == t.day && m == t.month
+      name.each do |row|
+        mgs.respond "Сегодня день рождения y #{row['ShipName']}"
+      end
+
+      year.each do |row|
+        mgs.respond "#{t.day.to_s}/#{t.month.to_s}/#{row['BirthdayYear']}"
+      end
+    else
+      f +=1
+    end
     i += 1
   end
 end
